0349fa71d5d31acc9eec413c23ff0dfcb46c1fa6
Dahub <dev.david.andre@gmail.com>
2014-09-07 22:14:15 +0200

    Amélioration stat

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -4,23 +4,23 @@ class StatsController < ApplicationController
 	def operation_distribution
 	end
 	
-	def operation_distribution_data
-		#Operation.joins(:operation_classification).group('operation_classifications.wording').sum(:amount)
-		#render :json => { :text => "Hello the world" }
-		#Date.new(params[:startDate])
+	def operation_distribution_data		
 		@my_bank_account = BankAccount.find(params[:bank_account_id])
 		if(check_if_user_own_bank_account(@my_bank_account))
 			render :json => {
-								:output => @my_bank_account.operations.where('date_operation >= ?', Date.parse(params[:startDate])).
-													where('date_operation < ?', Date.parse(params[:endDate])).output.
-													joins(:operation_classification).group('operation_classifications.wording').sum(:amount),
-								:input => @my_bank_account.operations.where('date_operation >= ?', Date.parse(params[:startDate])).
-													where('date_operation < ?', Date.parse(params[:endDate]))
-													.input.joins(:operation_classification).group('operation_classifications.wording').sum(:amount)
-							}
-			#render :json => { 	:label => "Legend",
-			#					:dataBar => Operation.joins(:operation_classification).group('operation_classifications.wording').sum(:amount)
-			#				}
+								:output => 	if(params[:outputs] == "true")
+												@my_bank_account.operations.where('date_operation >= ?', Date.parse(params[:startDate])).
+												where('date_operation < ?', Date.parse(params[:endDate])).output.
+												joins(:operation_classification).group('operation_classifications.wording').sum(:amount)
+											else nil
+											end	,
+								:input => 	if(params[:inputs] == "true")
+												@my_bank_account.operations.where('date_operation >= ?', Date.parse(params[:startDate])).
+												where('date_operation < ?', Date.parse(params[:endDate]))
+												.input.joins(:operation_classification).group('operation_classifications.wording').sum(:amount)
+											else nil
+											end
+							}			
 		end
 	end
 	
